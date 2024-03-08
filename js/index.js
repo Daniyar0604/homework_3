ee6a8cd6f243ec4cd182615c9c7b7e1d905828ea
Seytkumar_Daniyar <seytkumar980@gmail.com>
2024-03-08 20:26:58 +0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,242 @@
+// //Задание 1
+
+// const yodaSays = ["on JavaScript", "programming ", "I like "];
+
+// console.log(yodaSays[2], yodaSays[1], yodaSays[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=====================================================
+// Задание 3
+
+// let login = prompt('Введите ваш логин')
+
+// if (login === 'admin') {
+    
+// } else {
+//     alert('unknown user')
+// }
+
+// let password = prompt('Введите пароль')
+
+// if (password === 'BlackOverlord') {
+//     alert('Welcome')
+// } else {
+//     alert('Wrong password')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=====================================================
+// Задание 4
+
+// const napitok = ['milk', 'beer', 'beer', 'milk', 'milk']
+
+// for (let i = 0; i < napitok.length; i++) {
+//     const element = napitok [i];
+
+//     if (element === napitok[0, 3, 4] ) {
+//         console.log('good')
+
+//     } else {
+//         console.log('bad');
+//     }
+
+//     console.log(napitok);
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//=====================================================
+// Задание 5
+
+
+// const simvol = prompt('Введите &');
+
+// let ttt = '';
+
+// for (let i = 0; i < 7; i++) {
+
+//     for (let j = 0; j < 20; j++) {
+//         ttt += simvol;
+//     }
+    
+//     ttt += '\n'; 
+// }
+
+// alert(ttt);
+
+
+
+
+
+
+
+// ===========================================================
+// Задание 6
+
+
+// let vremya = prompt ('Напишите сколько осталось вам работать в секундах')
+
+
+// const bla = vremya / 3600
+
+
+// if (bla > 0) {
+//     alert (Math.floor(bla) )
+ 
+// } else {
+//     alert ('Не правильное значение')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//================================================================
+// Задание 7
+
+ 
+// const generatorOne = (Math.floor(Math.random () * 9 ) + 1)
+// const generatorTwo = (Math.floor(Math.random () * 9 ) + 1)
+
+// const rep = prompt ( 'What is' + (generatorOne) + '*' + (generatorTwo) )
+
+// result = generatorOne * generatorTwo
+
+
+// if ( result == rep) {
+//     alert ("You are correct!")
+
+// } else {
+//     alert ("You are wrong")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//============================================
+//Доп задание 1
+
+// const a = 8
+// const b = 8
+
+
+// if (a > b) {
+//     console.log(`${a} больше чем ${b}`);
+// } else if (a < b) {
+//     console.log(`${a} меньше чем ${b}`);
+// } else {
+//     console.log('данные числа равны');
+// }
+
+
+
+
+
+
+
+
+
+
+// ============================================
+// Доп задание 3
+
+// const a = 19
+
+// switch (a) {
+//     case a:
+//         const year = a / 1
+//         console.log(`вы живете ${year} лет`);
+   
+//     case a:
+//         const mounth = a * 12
+//         console.log(`вы живете ${mounth} месяцев`);
+    
+//     case day:
+//         const day = a * 365 
+//         console.log(`вы живете ${day} дней`);
+   
+//     case minut:
+//         const minut = a * 525600
+//         console.log(`вы живете ${minut} минут`);
+   
+//     case sec:
+//         const sec = a * 31536000
+//         console.log(`вы живете ${sec} секунд`);
+//     break;
+//     default:
+//         console.log('не правильное значение');
+//         break;
+// }
+
